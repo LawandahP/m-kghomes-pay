bd23df567ba3f321cda302e4b32c7a52c3a7f6b2
vader <pythongeeko@gmail.com>
2022-06-24 14:30:38 +0300

    swagger-ui🐱‍👤

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_yasg.views import get_schema_view as schema_view
+from drf_yasg import openapi
+
+
+schema_view = schema_view(
+   openapi.Info(
+      title="KgHomes Mpesa API",
+      default_version='0.1.0',
+      description="A simplified Mpesa api for KgHomesApp",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="sendpablo.gee@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+#    permission_classes=[permissions.AllowAny],
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mpesa.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
